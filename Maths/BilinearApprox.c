4c5892f7714fc56439760ca1accb7fa40f15da35
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-03-07 13:39:34 +0100

    Update BilinearApprox.c

--- Maths/BilinearApprox.c
+++ Maths/BilinearApprox.c
@@ -1,5 +1,6 @@
 // This program approximates the exponential function e^(At) using a rational function update rule.
 // This method is a special numerical approximation for exponentials, related to the bilinear (Tustin) transform.
+// It is closer to Euler's method but with a modified step correction.
 
 #include <stdio.h>
 #include <math.h>
